806ef51be24ae9c8b80e6e14e6a78fa09604bcdf
airon-main <airon.personal@proton.me>
2025-11-17 20:31:06 +0700

    LeaderBoard and UI overhaul

---
+++ app/dashboard/components/LeaderBoard.tsx
@@ -0,0 +1,62 @@
+import React, { PureComponent } from "react";
+
+export class LeaderBoard extends PureComponent {
+  render() {
+    return (
+      <div className="w-full card card-xs bg-base-200 p-4">
+        <span className="text-xl font-bold retext pb-2 pl-2">LeaderBoard</span>
+        {LeaderBoardItem.map((item, index) => (
+          <div key={index}>
+            <button className="w-full flex flex-row items-center justify-center hover:bg-base-300 transition duration-150 rounded-full">
+              <div className="avatar">
+                <div className="w-14 rounded-full">
+                  <img src={item.image} />
+                </div>
+              </div>
+              <div className="px-4 flex-1 flex flex-col items-start">
+                <span className="">{item.name} : {item.amount}</span>
+                <div className="badge my-1 badge-success">{item.change}</div>
+              </div>
+            </button>
+            {index != LeaderBoardItem.length - 1 && (
+              <div className="h-[0.5] rounded my-2 bg-base-300"></div>
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+const LeaderBoardItem = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=723&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "John Smith",
+    amount: "Rp100.000",
+    change: "+10%",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=723&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "John Smith",
+    amount: "Rp100.000",
+    change: "+10%",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=723&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "John Smith",
+    amount: "Rp100.000",
+    change: "+10%",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=723&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "John Smith",
+    amount: "Rp100.000",
+    change: "+10%",
+  },
+];
+
+export default LeaderBoard;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import LeaderBoard from "@/app/dashboard/components/LeaderBoard";
+import Link from "next/link";
 import React, { Component } from "react";
 
 export default class Dashboard extends Component {
@@ -10,50 +12,60 @@ export default class Dashboard extends Component {
           href="https://cdn.jsdelivr.net/npm/daisyui@latest/dist/full.min.css"
           rel="stylesheet"
         />
-        <div className="drawer drawer-open" data-theme="dim">
-          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col items-center">
-            <div className="p-2 w-full flex flex-col gap-2">
-              <div className="p-2 bg-base-200 rounded-box flex flex-row items-center">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,300,0,0"
+        />
+        <div data-theme="emerald">
+          <div className="w-full bg-leaf-300 flex flex-row">
+            <div className="flex flex-col">
+              <Link
+                href="/"
+                className="w-full px-4 pt-3 pb-2 flex justify-center items-center space-x-3 relogo"
+                onClick={() => {}}
+              >
+                <span className="self-center text-3xl font-semibold whitespace-nowrap text-leaf-900">
+                  Resapling
+                </span>
+              </Link>
+              <ul className="w-60 p-4 h-screen menu">
+                <li>
+                  <a className="text-leaf-900 bg-leaf-200 rounded-full" href="">
+                    <span className="material-symbols-rounded">dashboard</span>
+                    Dashboard
+                  </a>
+                  <a className="text-leaf-900 rounded-full" href="">
+                    <span className="material-symbols-rounded">volunteer_activism</span>
+                    Buat Komunitas
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="w-full p-4 h-screen bg-leaf-50 flex flex-col gap-4">
+              {/* Content */}
+              <div className="w-full p-2 bg-base-200 rounded-box flex flex-row items-center">
                 <div className="w-16 rounded-full overflow-hidden">
                   <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
                 </div>
-                <div className="px-4">
+                <div className="px-4 flex-1">
                   <p className="text-lg">Welcome Aaron</p>
                   <div className="badge badge-warning">Warning</div>
                 </div>
+                <button className="btn btn-square btn-ghost">
+                  <span className="material-symbols-rounded">edit</span>
+                </button>
               </div>
-              <div className="flex flex-row gap-2">
-                <div className="flex flex-1 flex-col gap-2">
-                  <div className="flex flex-col gap-2 rounded-box bg-base-200 p-2">
-                    <div className="flex flex-row">Lorem Ipsum</div>
-                  </div>
+              <div className="flex flex-row gap-4">
+                {/* Left */}
+                <div className="flex-1">
+                  <LeaderBoard />
                 </div>
-                <div className="flex flex-1 flex-col gap-2">
-                  <div className="rounded-box bg-base-200 p-2">Gurt : Yo</div>
+                {/* Right */}
+                <div className="flex-1">
+
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="drawer-side">
-            <ul className="menu bg-base-200 min-h-full w-60 p-4">
-              <li>
-                <a className="bg-primary text-primary-content">
-                  <span className="material-symbols-rounded">dashboard</span>
-                  Dashboard
-                </a>
-                <a>
-                  <span className="material-symbols-rounded">home</span>Home
-                </a>
-                <a>
-                  <span className="material-symbols-rounded">
-                    volunteer_activism
-                  </span>
-                  Buat Komunitas
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
       </>
